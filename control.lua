d4953b43442dd89705581a67204db713e29dbf27
Thomas Deal <justarandomgeek@gmail.com>
2017-06-11 10:17:05 -0400

    simple text box for import/export json data

--- control.lua
+++ control.lua
@@ -1,5 +1,4 @@
 local json = require('dkjson')
---TODO: create in-game editor interface and import/export strings for tapes
 
 local signalblacklist = {["signal-diskreader-read"]=true, ["signal-diskreader-write"]=true}
 
@@ -9,8 +8,8 @@ local function onTickManager(manager)
     manager.cc2.get_or_create_control_behavior().parameters=nil
   end
 
-  local tape = manager.ent.get_inventory(defines.inventory.furnace_source)[1]
-  if tape and tape.valid_for_read and tape.name=="disk" then
+  local disk = manager.ent.get_inventory(defines.inventory.furnace_source)[1]
+  if disk and disk.valid_for_read and disk.name=="disk" then
 
     -- read cc1 signals. Only uses one wire, red if both connected.
     local signet1 = manager.cc1.get_circuit_network(defines.wire_type.red) or manager.cc1.get_circuit_network(defines.wire_type.green)
@@ -18,15 +17,15 @@ local function onTickManager(manager)
       local readsig = signet1.get_signal({name="signal-diskreader-read",type="virtual"})
       if readsig ~= 0 then
         if readsig == -1 then
-          -- eject tape
-          manager.ent.get_inventory(defines.inventory.furnace_result)[1].set_stack(tape)
-          tape.clear()
+          -- eject disk
+          manager.ent.get_inventory(defines.inventory.furnace_result)[1].set_stack(disk)
+          disk.clear()
 
         elseif readsig > 0 and readsig <= 512 then
           -- read a frame to cc2
-          local tapedata = tape.get_tag("disk_"..readsig)
-          if tapedata then
-            manager.cc2.get_or_create_control_behavior().parameters={parameters = tapedata}
+          local diskdata = disk.get_tag("disk_"..readsig)
+          if diskdata then
+            manager.cc2.get_or_create_control_behavior().parameters={parameters = diskdata}
             manager.clearcc2 = true
           end
 
@@ -42,9 +41,9 @@ local function onTickManager(manager)
             end
           end
           if #storeframe > 0 then
-            tape.set_tag("disk_"..writesig, storeframe)
+            disk.set_tag("disk_"..writesig, storeframe)
           else
-            tape.set_tag("disk_"..writesig, nil)
+            disk.set_tag("disk_"..writesig, nil)
           end
 
         end
@@ -105,25 +104,65 @@ local function onBuilt(event)
   end
 end
 
-local function ExportTape(tape)
+local function ExportDisk(disk)
   local data = {}
   for i = 1,512 do
-    data[tostring(i)] = tape.get_tag("disk_"..i)
+    data[tostring(i)] = disk.get_tag("disk_"..i)
   end
-  return json.encode{label=tape.label, data=data}
+  return json.encode{label=disk.label, data=data}
 end
 
-local function ImportTape(tape,data)
+local function ImportDisk(disk,data)
   local decode = json.decode(data)
-  tape.label = decode.label
-  tape.set_tag("disk",decode.data)
+  if decode then
+    if decode.label then disk.label = decode.label end
+    if decode.data then
+      for i = 1,512 do
+        disk.set_tag("disk_"..i,decode.data[tostring(i)])
+      end
+    end
+    return true
+  else
+    return false
+  end
+end
+
+local function onCursorStackChanged(event)
+  local player = game.players[event.player_index]
+  if player.cursor_stack.valid_for_read and player.cursor_stack.name == "disk" then
+    local frame = player.gui.left.add{type="frame", name="diskgui"}
+  	local textbox = frame.add{type="textfield", name="disk-string-text"}
+    textbox.text = ExportDisk(player.cursor_stack)
+  else
+    local frame = player.gui.left["diskgui"]
+    if frame then
+      frame.destroy()
+    end
+  end
+end
+
+local function onTextChanged(event)
+  if event.element.name=="disk-string-text" then
+    local player = game.players[event.player_index]
+    if ImportDisk(player.cursor_stack,event.element.text) then
+      player.print("DISK updated!")
+    end
+  end
 end
 
 script.on_event(defines.events.on_tick, onTick)
 script.on_event(defines.events.on_built_entity, onBuilt)
 script.on_event(defines.events.on_robot_built_entity, onBuilt)
+script.on_event(defines.events.on_player_cursor_stack_changed, onCursorStackChanged)
+script.on_event(defines.events.on_gui_text_changed, onTextChanged)
+--on_gui_checked_state_changed
+--on_gui_click
+--on_gui_elem_changed
+--on_gui_selection_state_changed
+
+
 
 remote.add_interface('disk',{
-  export = ExportTape,
-  import = ImportTape,
+  export = ExportDisk,
+  import = ImportDisk,
 })
